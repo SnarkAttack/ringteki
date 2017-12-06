3fe318f7f09f4cd95c1bbf28e99d6def80992557
Patrick McQuay <patrick.mcquay@gmail.com>
2017-12-05 22:40:25 -0500

    Implemented Crisis Breaker

--- server/game/cards/02.5-FHNS/CrisisBreaker.js
+++ server/game/cards/02.5-FHNS/CrisisBreaker.js
@@ -1,10 +1,26 @@
 const DrawCard = require('../../drawcard.js');
 
 class CrisisBreaker extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Ready and bring into play',
+            condition: this.game.currentConflict && this.game.currentConflict.conflictType === 'military' &&
+                        ((this.game.currentConflict.attackingPlayer === this.controller && this.game.currentConflict.attackerSkill < this.game.currentConflict.defenderSkill) ||
+                        (this.game.currentConflict.defendingPlayer === this.controller && this.game.currentConflict.defenderSkill < this.game.currentConflict.attackerSkill)),
+            target: {
+                activePromptTitle: 'Choose a character',
+                cardType: 'character',
+                cardCondition: card => card.location === 'play area' && !card.isParticipating() && card.bowed && card.hasTrait('berserker')
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to ready {2} and move it into the conflict', this.controller, this, context.target);
+                this.controller.readyCard(context.target, this);
+                this.game.currentConflict.moveToConflict(context.target);
+            }
+        });
     }
 }
 
-CrisisBreaker.id = 'crisis-breaker'; // This is a guess at what the id might be - please check it!!!
+CrisisBreaker.id = 'crisis-breaker';
 
 module.exports = CrisisBreaker;
